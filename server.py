27a6b00c1b539dd2bf6e8b1325e4667786217b12
Olivier PEDOUSSAUT <opedoussaut@gmail.com>
2025-06-03 12:48:33 +0200

    Your commit message

--- server.py
+++ server.py
@@ -1,23 +1,19 @@
+from dotenv import load_dotenv
 import os
-from pymongo import MongoClient
-from flask import Flask, jsonify, request
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-app = Flask(__name__)
+# Load environment variables from .env
+load_dotenv()
 
-mongo_user = os.getenv("MONGO_USER")
-mongo_password = os.getenv("MONGO_PASSWORD")
-mongo_uri = f"mongodb+srv://{mongo_user}:{mongo_password}@ecoinventdb.zss85ou.mongodb.net/"
-client = MongoClient(mongo_uri)
-db = client["ecoinventDB"]
-collection = db["cutoff_activities"]
+# Get connection details
+uri = os.getenv('MONGO_URI')
 
-@app.route('/activities', methods=['GET'])
-def get_activities():
-    query = {}
-    result = list(collection.find(query).limit(10))
-    for doc in result:
-        doc['_id'] = str(doc['_id'])
-    return jsonify(result)
+# Connect to MongoDB
+client = MongoClient(uri, server_api=ServerApi('1'))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+try:
+    client.admin.command('ping')
+    print("✅ Successfully connected to MongoDB!")
+except Exception as e:
+    print("❌ Connection failed:", e)
